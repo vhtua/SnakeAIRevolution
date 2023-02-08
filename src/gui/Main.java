52e45383c3ab2dfc5d5f6293eb689c98e9000541
vhtuananh020402 <vhtuananh020402@gmail.com>
2023-02-08 12:53:09 +0100

    up

---
+++ src/gui/Main.java
@@ -0,0 +1,9 @@
+package gui;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StartScreen newGame = new StartScreen();
+        newGame.run();
+    }
+}
+
